f55f8b8fdb1df5f8f873c8a4a0149221d9b6847f
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2024-03-24 21:21:31 -0500

    remove accessibility 😎💀

--- Root.tsx
+++ Root.tsx
@@ -11,6 +11,25 @@ import { store } from "./components/core/state/store";
 import { Provider } from "react-redux";
 import ErrorFallback from "./components/util/ErrorFallback";
 import ErrorBoundary from "./components/util/ErrorBoundary";
+import { Text, TextInput } from "react-native";
+
+interface TextWithDefaultProps extends Text {
+  defaultProps?: { allowFontScaling?: boolean };
+}
+
+interface TextInputWithDefaultProps extends TextInput {
+  defaultProps?: { allowFontScaling?: boolean };
+}
+
+(Text as unknown as TextWithDefaultProps).defaultProps =
+  (Text as unknown as TextWithDefaultProps).defaultProps || {};
+(Text as unknown as TextWithDefaultProps).defaultProps!.allowFontScaling =
+  false;
+(TextInput as unknown as TextInputWithDefaultProps).defaultProps =
+  (TextInput as unknown as TextInputWithDefaultProps).defaultProps || {};
+(
+  TextInput as unknown as TextInputWithDefaultProps
+).defaultProps!.allowFontScaling = false;
 
 export default function Root() {
   const [key, setKey] = useState(0);
